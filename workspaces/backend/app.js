dc7e54d32c0242cd6830256bc7b576af7ded5ee4
Andrey A. Kiselev <Andrey.A.Kiselev@kaspersky.com>
2024-06-18 11:47:12 +0300

    init

---
+++ workspaces/backend/app.js
@@ -0,0 +1,3 @@
+import { router } from "contracts";
+
+console.log(typeof router);
---
+++ workspaces/backend/import-fix.js
@@ -0,0 +1,3 @@
+import { register } from "node:module";
+
+register(import.meta.resolve("import-in-the-middle/hook.mjs"));
