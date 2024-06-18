e8490482076e1ecb16e9f9f62a3d549dcdac0ebc
Andrey A. Kiselev <Andrey.A.Kiselev@kaspersky.com>
2024-06-18 13:00:12 +0300

    update fix

--- import-fix.js
+++ import-fix.js
@@ -1,3 +1,3 @@
 import { register } from "node:module";
 
-register(import.meta.resolve("import-in-the-middle/hook.mjs"));
+register(import.meta.resolve("import-in-the-middle/hook.mjs"), import.meta.url);
